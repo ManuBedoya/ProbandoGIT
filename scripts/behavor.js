ea997458b508fd251fbb9f0d989dad0f5da9dfdd
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2020-10-05 21:19:44 -0500

    Nueva menu e imagen en la seccion lateral

--- scripts/behavor.js
+++ scripts/behavor.js
@@ -1,5 +1,5 @@
 let cabecera =
-  " <nav> <a href='index.html'>Inicio</a> <a href='cursos.html'>Cursos</a> <a href='#'>Articulos</a> <a href='paginas.html'>Paginas</a> <a href='#'>Contacto</a> </nav>";
+  " <nav> <a href='index.html'>Inicio</a> <a href='cursos.html'>Cursos</a> <a href='#'>Articulos</a> <a href='paginas.html'>Paginas</a> <a href='#'>Contacto</a> <a href='#'>PruebaGIT</a> </nav>";
 let footer =
   " <div id='socialMedia'><a href='https://www.instagram.com/manuelbedoya5600/' target='_blanck' class= 'fa fa-instagram'></a><a target='_blanck' class='colorRed' href='https://www.instagram.com/manuelbedoya5600/'>Contactame en Instagram</a>&nbsp;&nbsp;&nbsp; <a   class= 'fa fa-twitter' target='_blanck' href='https://twitter.com/ManuelB45158741'></a><a target='_blanck' class='colorRed ' href='https://twitter.com/ManuelB45158741'>Contactame en twitter</a> &nbsp;&nbsp;&nbsp;<a target='_blanck' href='https://www.linkedin.com/in/manuel-fernando-bedoya-garcia-ba33971b5/' class= 'fa fa-linkedin'/><a target='_blanck' class='colorRed ' href='https://www.linkedin.com/in/manuel-fernando-bedoya-garcia-ba33971b5/'>Contactame en Linkedin</a> &nbsp;&nbsp;&nbsp;  <a target='_blanck' class= 'fa fa-google'/><a target='_blanck' class='colorRed ' href=''> Contacto al correo</a> </div>";
 
